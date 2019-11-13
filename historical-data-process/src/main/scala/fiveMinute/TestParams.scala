753cf14b05a3983d7128ee12b2a6b5c50a432f2a
Qanat <kmergenbayev@gmail.com>
2019-11-13 10:26:28 +0600

    fix test params case class.

--- historical-data-process/src/main/scala/fiveMinute/TestParams.scala
+++ historical-data-process/src/main/scala/fiveMinute/TestParams.scala
@@ -1,4 +1,17 @@
 package fiveMinute
 
 
-case class TestParams()
+case class TestParams(benchmark_negative: Option[Double],
+                      benchmark_positive: Option[Double],
+                      limit_price: Double,
+                      limit_time: Int,
+                      limit_trades: Int,
+                      min_trail_percent: Double,
+                      stop_trades: Int,
+                      trail_percent: Double,
+                      trail_time: Int,
+                      high_delta: Option[Boolean],
+                      low_delta: Option[Boolean],
+                      low_stop: Option[Boolean],
+                      high_stop: Option[Boolean],
+                      spy: Boolean)
